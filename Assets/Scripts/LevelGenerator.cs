cc586c794cf90415e647dabce3e5613a0c7f1a42
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-03 15:42:59 +0200

    Add Generator levels and move camera

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,19 +8,23 @@ public class LevelGenerator : MonoBehaviour
     public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
     public Transform levelStartPoint;
     public List<LevelBlock> currentBlocks = new List<LevelBlock>();
+    public int initialBlocks = 2;
     private void Awake()
     {
         sharedInstance = this;
     }
     private void Start()
     {
-        this.AddLevelBlock();
+        this.GenerateInitialBlocks();     
     }
 
     public void AddLevelBlock()
     {
+        // Generar un numero aleatorio entre a y b
         int randomIndex = Random.Range(0, this.allTheLevelBlocks.Count);
+        // Creamos una copia del bloque de nivel desde la carpeta assets hasta la escena
         LevelBlock currentBlock = (LevelBlock)Instantiate(allTheLevelBlocks[randomIndex]);
+        // Pone el nuevo bloque de nivel como hijo del Level Generator
         currentBlock.transform.SetParent(this.transform, false);
         Vector3 spawnPosition = Vector3.zero;
 
@@ -32,7 +36,13 @@ public class LevelGenerator : MonoBehaviour
         {
             spawnPosition = this.currentBlocks[this.currentBlocks.Count - 1].exitPoint.position;
         }
-        currentBlock.transform.position = spawnPosition;
+
+        Vector3 correction = new Vector3(
+            spawnPosition.x - currentBlock.startPoint.position.x,
+            spawnPosition.y - currentBlock.startPoint.position.y,
+            0
+        );
+        currentBlock.transform.position = correction;
         this.currentBlocks.Add(currentBlock);
     }
 
@@ -48,6 +58,9 @@ public class LevelGenerator : MonoBehaviour
 
     public void GenerateInitialBlocks()
     {
-
+        for (int i = 0; i < this.initialBlocks; i++)
+        {
+            this.AddLevelBlock();
+        }
     }
 }
