9cbee2997024e93bf38d4737e63621c73921418c
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-04 22:19:04 +0200

    Add enemy move nevel

---
+++ Assets/Scripts/TriggerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMovement : MonoBehaviour
+{
+    public bool movingForward;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (this.movingForward)
+        {
+            Enemy.turnAround = true;
+            print("Hola");
+        }
+        else
+        {
+            Enemy.turnAround = false;
+        }
+        this.movingForward = !this.movingForward;
+    }
+}
